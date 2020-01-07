8e85f213612d64821ec2be7c24ba0cf4cba9a940
bpohl <github@bpohl.com>
2020-01-06 20:51:54 -0600

    Fixed env var bug. More comments. Added -q flag to limit email from cron.

--- drobo-mount.sh
+++ drobo-mount.sh
@@ -2,21 +2,28 @@
 # Mount the Drobo if it isn't mounted and if another process isn't
 # already trying.
 
-: ${MOUNT_POINT:="/media/Drobo"}
+: ${DROBO_MOUNTPOINT:="/media/Drobo"}
 : ${PID_FILE:="/run/drobo-mount.pid"}
 
-if findmnt --df --first "$DROBO_MOUNT_POINT"; then
-  echo "Warning: $DROBO_MOUNT_POINT already mounted." >&2
+[ "$1" == "-q" ] && exec >/dev/null
+
+# No use if the disk is already mounted
+if findmnt --df --first "$DROBO_MOUNTPOINT"; then
+  echo "$DROBO_MOUNTPOINT already mounted." 
   exit 1
 fi
 
+# Make sure another process isn't already trying
 unset old_pid
 if ps ${old_pid:=$(cat "$PID_FILE")} 999999999; then
-  echo "Warning: drobo-mount already running at PID $old_pid." >&2
+  echo "Drobo mounting script already running at PID $old_pid." 
   exit 2
 fi
 echo $$ > "$PID_FILE"
 
-exec mount "$DROBO_MOUNT_POINT"
+# Let the mount command take over the process
+echo "Mounting $DROBO_MOUNTPOINT" >&2
+exec mount "$DROBO_MOUNTPOINT"
 
+# Should never get here
 exit 3
